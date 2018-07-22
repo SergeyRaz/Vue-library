052187d2d99e8556f3e0be3a1a7aea8f46174dea
SergeyRazzhivin <razjiwin2010@yandex.ru>
2018-07-22 21:49:03 +0300

    Vue-library - Galery version 1.6 - View elements carousel variant - 1

--- src/assets/components/gallery/Carousel.vue
+++ src/assets/components/gallery/Carousel.vue
@@ -1,43 +1,58 @@
 <template>
 	<div class="carousel">
-		<img class="carousel-img"
+		<div class="carousel-block"
+		:style="shift">
+			<img class="carousel-img"
       v-for="(value, index, key) in arrImg"
       :class="{active : value.status}"
       :key="key"
       :src="value.img" 
       :alt="key"
 			@click="onView(index)">
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	props: ['arrImg'],
+  props: ["arrImg"],
   data() {
     return {
-			a: 1
+      shift: ''
     };
-	},
-	methods: {
-		onView(index){
-			this.$emit('onView', index);
-		}
-	}
+  },
+  methods: {
+    onView(index) {
+      if(index < (this.arrImg.length - 2)){
+        this.shift = 'left: -' + (153*(index-1)) + 'px;'
+      }
+      this.$emit("onView", index);
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.carousel{
-	display: flex;
-	.carousel-img{
-		box-sizing: border-box;
-		width: 25%;
+.carousel {
+	height: 85px;
+	position: relative;
+	overflow: hidden;
+  .carousel-block {
+		position: absolute;
+    display: flex;
 		height: 85px;
-    border-right: 3px solid #fff;
-		cursor: pointer;
-		&:last-child{
-			border: none;
-		}
-	}
+		transition: 1s;
+    left: 0;
+    .carousel-img {
+      box-sizing: border-box;
+      width: 151px;
+      height: 85px;
+      border-right: 3px solid #fff;
+      cursor: pointer;
+      &:last-child {
+        border: none;
+      }
+    }
+  }
 }
 </style>
